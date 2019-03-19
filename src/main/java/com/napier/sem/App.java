dd50954d24a3481fc09adb6af110658d74017243
LiamMBailey <46745949+LiamMBailey@users.noreply.github.com>
2019-03-19 11:42:08 +0000

    Commit to add code of conduct.

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -7,7 +7,6 @@ public class App {
 
     public static void main(String[] args) {
 
-
         SqlServerConnection sql = SqlServerConnection.getInstance();
         Console console = System.console();
         String sqlStatement;
