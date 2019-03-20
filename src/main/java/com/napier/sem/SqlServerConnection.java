d8e0a3c6235cdf4bd5462142699dd572090d9060
harik <harikharel7@gmail.com>
2019-03-20 10:48:03 +0000

    Fixed some warnings

--- src/main/java/com/napier/sem/SqlServerConnection.java
+++ src/main/java/com/napier/sem/SqlServerConnection.java
@@ -18,7 +18,7 @@ public class SqlServerConnection {
     public void connect(String location) {
         try {
             // Load Database driver
-            Class.forName("com.mysql.jdbc.Driver");
+            Class.forName("com.mysql.cj.jdbc.Driver");
         } catch (ClassNotFoundException e) {
             System.out.println("Could not load SQL driver");
             System.exit(-1);
