f5ce6c4d9afea08f365926ac957c7753bd42203e
LiamMBailey <40316961@live.napier.ac.uk>
2019-03-20 10:39:07 +0000

    Add city and capital city classes

---
+++ src/main/java/com/napier/sem/CapitalCity.java
@@ -0,0 +1,43 @@
+package com.napier.sem;
+
+public class CapitalCity
+{
+    private String _capitalCityName;
+    private String _capitalCityCountry;
+    private int _capitalCityPopulation;
+
+    public CapitalCity(String capitalCityName, String capitalCityCountry, int capitalCityPopulation)
+    {
+        _capitalCityName = capitalCityName;
+        _capitalCityCountry = capitalCityCountry;
+        _capitalCityPopulation = capitalCityPopulation;
+    }
+
+    // Getter and setter for Name
+    public String getCapitalCityName() {
+        return _capitalCityName;
+    }
+
+    public void setCapitalCityName(String capitalCityName) {
+        _capitalCityName = capitalCityName;
+    }
+
+    // Getter and setter for Country
+    public String getCapitalCityCountry() {
+        return _capitalCityCountry;
+    }
+
+    public void setCapitalCityCountry(String capitalCityCountry) {
+        _capitalCityCountry = capitalCityCountry;
+    }
+
+    // Getter and setter for Population
+    public int getCapitalCityPopulation() {
+        return _capitalCityPopulation;
+    }
+
+    public void setCapitalCityPopulation(int capitalCityPopulation) {
+        _capitalCityPopulation = capitalCityPopulation;
+    }
+
+}
