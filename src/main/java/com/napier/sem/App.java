9eef0f25907ebb51c71a921009f33e3964a2f9e1
harik <harikharel7@gmail.com>
2019-04-03 18:02:13 +0100

    Codecov integration

--- src/main/java/com/napier/sem/App.java
+++ src/main/java/com/napier/sem/App.java
@@ -3,17 +3,12 @@ package com.napier.sem;
 
 import com.napier.sem.sqlserver.RequirementsSQL;
 import com.napier.sem.sqlserver.SqlServerConnection;
-import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.web.bind.annotation.RestController;
 
 import java.sql.Connection;
 import java.util.List;
 import java.util.Scanner;
 import java.io.Console;
 
-@SpringBootApplication
-@RestController
 public class App {
 
     public static void main(String[] args) {
