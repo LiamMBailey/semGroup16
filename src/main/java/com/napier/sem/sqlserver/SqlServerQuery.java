d16e26cc98874c42c53b5f913d0f1f638ede6ba1
LiamMBailey <40316961@live.napier.ac.uk>
2019-04-05 17:00:49 +0100

    set endpoint to local host

--- src/main/java/com/napier/sem/sqlserver/SqlServerQuery.java
+++ src/main/java/com/napier/sem/sqlserver/SqlServerQuery.java
@@ -17,7 +17,7 @@ public class SqlServerQuery {
 
     @Autowired
     public SqlServerQuery(SqlServerConnection sqlServerConnection) {
-        String location = "35.242.185.153:80";
+        String location = "localhost:33060";
 
         this.sqlServerConnection = sqlServerConnection;
         con = sqlServerConnection.connect(location);
